8e105967e89c25f981e6cfc6ec5b07b2983ea368
ERKAN KOYUN <77962856+erkankoyun@users.noreply.github.com>
2026-08-14 12:31:23 -0400

    Make feature tests independent of Vite build assets

--- tests/Feature/AdminAccessTest.php
+++ tests/Feature/AdminAccessTest.php
@@ -5,6 +5,10 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
 
+beforeEach(function () {
+    $this->withoutVite();
+});
+
 it('allows anyone to browse products', function () {
     $this->get(route('products.index'))->assertOk();
 });
