487488aa803f1d76fc49b1270f1bb2127b898895
ERKAN KOYUN <77962856+erkankoyun@users.noreply.github.com>
2026-08-14 12:24:58 -0400

    Add Product model

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'is_available',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'price' => 'decimal:2',
+            'is_available' => 'boolean',
+        ];
+    }
+}
