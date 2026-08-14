4d9953f4132cde89cc9dafa2ca246509fa6b5538
ERKAN KOYUN <77962856+erkankoyun@users.noreply.github.com>
2026-08-14 15:28:29 -0400

    Use relative auth redirects for proxied environments

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,10 +32,10 @@ class AuthController extends Controller
         $request->session()->regenerate();
 
         if ($request->user()?->is_admin) {
-            return redirect()->intended(route('admin.dashboard'));
+            return redirect()->intended(route('admin.dashboard', absolute: false));
         }
 
-        return redirect()->intended(route('products.index'));
+        return redirect()->intended(route('products.index', absolute: false));
     }
 
     public function showRegister(): View
@@ -55,7 +55,7 @@ class AuthController extends Controller
         Auth::login($user);
         $request->session()->regenerate();
 
-        return redirect()->route('products.index')
+        return redirect()->to(route('products.index', absolute: false))
             ->with('success', 'Account created successfully.');
     }
 
@@ -66,6 +66,6 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect()->route('home');
+        return redirect()->to(route('home', absolute: false));
     }
 }
